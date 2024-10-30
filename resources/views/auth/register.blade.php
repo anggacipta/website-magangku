01f1a21a94e833a0dbf692daa4b28c442cc42b58
anggacipta <anggasusanto12@gmail.com>
2024-10-30 12:39:41 +0700

    checkpoint for register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,52 +1,90 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Register</title>
+
     <link rel="stylesheet" href="{{ asset('assets/adminlte/plugins/bootstrap-5/css/bootstrap.min.css') }}">
 </head>
 <body>
-<div class="container">
-    <h2>Register</h2>
-    <form method="POST" action="{{ route('register') }}">
-        <!-- CSRF Token -->
-        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+<section class="h-100">
+    <div class="container h-100">
+        <div class="row justify-content-sm-center h-100">
+            <div class="col-xxl-4 col-xl-5 col-lg-5 col-md-7 col-sm-9">
+                <div class="text-center my-5">
+                    <img src="https://getbootstrap.com/docs/5.0/assets/brand/bootstrap-logo.svg" alt="logo" width="100">
+                </div>
+                <div class="card shadow-lg">
+                    <div class="card-body p-5">
+                        <h1 class="fs-4 card-title fw-bold mb-4">Register</h1>
+                        <form method="POST" class="needs-validation" action="{{ route('register') }}" autocomplete="off">
+                            @csrf
+                            <div class="mb-3">
+                                <label class="mb-2 text-muted" for="email">Nama lengkap:</label>
+                                <input id="email" type="text" class="form-control" name="name" required>
+                                <div class="invalid-feedback" style="display: block;">
+                                    {{ $errors->first('name') }}
+                                </div>
+                            </div>
 
-        <!-- Name -->
-        <div>
-            <label for="name">Name</label>
-            <input id="name" type="text" name="name" required autofocus>
-            <span class="error">{{ $errors->first('name') }}</span>
-        </div>
+                            <div class="mb-3">
+                                <label class="mb-2 text-muted" for="email">E-Mail Address</label>
+                                <input id="email" type="email" class="form-control" name="email" value="" required="" autofocus="">
+                                <div class="invalid-feedback" style="display: block;">
+                                    {{ $errors->first('email') }}
+                                </div>
+                            </div>
 
-        <!-- Email Address -->
-        <div>
-            <label for="email">Email</label>
-            <input id="email" type="email" name="email" required>
-            <span class="error">{{ $errors->first('email') }}</span>
-        </div>
+                            <div class="mb-3">
+                                <div class="mb-2 w-100">
+                                    <label class="text-muted" for="password">Password</label>
+                                    <a href="#" class="float-end">
+                                        Forgot Password?
+                                    </a>
+                                </div>
+                                <input id="password" type="password" class="form-control" name="password" required="">
+                                <div class="invalid-feedback" style="display: block;">
+                                    {{ $errors->first('password') }}
+                                </div>
+                            </div>
 
-        <!-- Password -->
-        <div>
-            <label for="password">Password</label>
-            <input id="password" type="password" name="password" required>
-            <span class="error">{{ $errors->first('password') }}</span>
-        </div>
+                            <div class="mb-3">
+                                <div class="mb-2 w-100">
+                                    <label class="text-muted" for="password">Password Confirmation</label>
+                                    <a href="#" class="float-end">
+                                        Forgot Password?
+                                    </a>
+                                </div>
+                                <input id="password" type="password" class="form-control" name="password_confirmation" required="">
+                                <div class="invalid-feedback" style="display: block;">
+                                    {{ $errors->first('password_confirmation') }}
+                                </div>
+                            </div>
 
-        <!-- Confirm Password -->
-        <div>
-            <label for="password_confirmation">Confirm Password</label>
-            <input id="password_confirmation" type="password" name="password_confirmation" required>
-            <span class="error">{{ $errors->first('password_confirmation') }}</span>
+                            <div class="d-flex align-items-center">
+                                <button type="submit" class="btn btn-primary ms-auto">
+                                    Login
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="card-footer py-3 border-0">
+                        <div class="text-center">
+                            Don't have an account? <a href="{{ route('register') }}" class="text-dark">Create One</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="text-center mt-5 text-muted">
+                    Copyright © 2017-2021 — Your Company
+                </div>
+            </div>
         </div>
+    </div>
+</section>
 
-        <!-- Submit Button -->
-        <div>
-            <button type="submit">Register</button>
-        </div>
-    </form>
-    <p>Already registered? <a href="/login">Login</a></p>
-</div>
+<script type="text/javascript" src="{{ asset('assets/adminlte/plugins/bootstrap-5/js/bootstrap.min.js') }}"></script>
 </body>
 </html>
